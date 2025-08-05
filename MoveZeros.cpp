b6f9a8fcb2aefcc3d2f5b9f3843b16ba0ab1648c
Harsh Khapekar <harshkhapekar1@gmail.com>
2025-08-05 19:56:41 +0530

    Create MoveZeros.cpp

---
+++ MoveZeros.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int j=0;
+        for(int i=0 ;i<n;i++){
+            if(nums[i] != 0){
+                swap(nums[j] , nums[i]);
+                j++;
+            }
+        }
+        return nums;
+        
+    }
+};
